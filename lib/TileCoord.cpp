afa6b0a840a97fe32f0281303dbe0b409390bc4f
Cecilia Norevik Bratlie <cecilia_nor@hotmail.com>
2024-04-07 14:41:15 +0200

    docs: add doc comments to TileCoord.cpp and TileCoord.h
    Updates: #89

--- lib/TileCoord.cpp
+++ lib/TileCoord.cpp
@@ -1,5 +1,14 @@
 #include "TileCoord.h"
 
+/*!
+ * \brief TileCoord::toString generates a string representing a TileCoord.
+ *
+ * This string is used when rendering debug information in the application.
+ *
+ * \return coordiante string on the form 'zoom: zoom (x coordinate, y coordinate)'.
+ *
+ * Example return string: "zoom 1 (1, 0)".
+ */
 QString TileCoord::toString() const
 {
     // Create formatted string based on the members.
@@ -9,6 +18,14 @@ QString TileCoord::toString() const
         .arg(y);
 }
 
+/*!
+ * \brief TileCoord::operator < defines the less than operator: '<'.
+ *
+ * The operator can now be used as a key in QMap.
+ *
+ * \param other is the TileCoord to compare the current TileCoord to.
+ * \return true if the current TileCoord values are less than the zoom, x, y of the 'other'.
+ */
 bool TileCoord::operator<(const TileCoord &other) const
 {
     if (zoom < other.zoom)
@@ -27,11 +44,28 @@ bool TileCoord::operator<(const TileCoord &other) const
         return false;
 }
 
+
+/*!
+ * \brief TileCoord::operator == defines the equality operator: '=='.
+ *
+ * The operator can now be used as a key in QMap.
+ *
+ * \param other is the TileCoord to compare the current TileCoord to.
+ * \return true if zoom, x, y are the same for two `TileCoord`s.
+ */
 bool TileCoord::operator==(const TileCoord& other) const
 {
     return zoom == other.zoom && x == other.x && y == other.y;
 }
 
+/*!
+ * \brief TileCoord::operator != defines the inequality operator: '!='.
+ *
+ *  The operator can now be used as a key in QMap.
+ *
+ * \param other is the TileCoord to compare the current TileCoord to.
+ * \return true if zoom, x, y are not the same for two `TileCoord`s.
+ */
 bool TileCoord::operator!=(const TileCoord& other) const
 {
     return !(*this == other);
