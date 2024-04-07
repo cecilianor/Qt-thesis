c7257912747b6a5cc88196bd1a337d1f5bc53e4a
Cecilia Norevik Bratlie <cecilia_nor@hotmail.com>
2024-04-07 17:37:01 +0200

    docs: add QDoc comments and other comments to NotImplementedStyle::fromJson(...)
    Updates: #93

--- lib/Layerstyle.cpp
+++ lib/Layerstyle.cpp
@@ -656,6 +656,16 @@ QVariant SymbolLayerStyle::getTextOpacityAtZoom(int zoomLevel) const
     }
 }
 
+/*!
+ * \brief NotImplementedStyle::fromJson returns the NotImplementedStyle
+ * layer style.
+ *
+ * This is used for any layer with type value other than "background",
+ * "fill", "line", or "symbol".
+ *
+ * \param jsonObj is a JSonObject. It's passed here to match Layer style types.
+ * \return the NotImplementedStyle.
+ */
 NotImplementedStyle* NotImplementedStyle::fromJson(const QJsonObject &jsonObj)
 {
     NotImplementedStyle* returnLayer = new NotImplementedStyle();
