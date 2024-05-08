5c7821deb6d021951a49306035fde1329597ee03
Cecilia Norevik Bratlie <cecilia_nor@hotmail.com>
2024-05-08 17:51:10 +0200

    style/docs: add and update documentation to MapCoordControlWidget.h and re-style the code
    private members and methods are now placed above public ones.
    
    Updates: #121

--- app/MapCoordControlWidget.h
+++ app/MapCoordControlWidget.h
@@ -1,7 +1,7 @@
 #ifndef MAPCOORDCONTROLWIDGET_H
 #define MAPCOORDCONTROLWIDGET_H
 
-// Qt header files
+// Qt header files.
 #include <QBoxLayout>
 #include <QLineEdit>
 #include <QWidget>
@@ -9,37 +9,46 @@
 // Other header files
 #include "MapWidget.h"
 
-namespace Bach {
+namespace Bach
+{
+/*!
+ * \class MapCoordControlWidget
+ * \brief The MapCoordControlWidget class is a collection
+ * of controls that modify the location and zoom of the
+ * MapWidget's viewport.
+ */
+class MapCoordControlWidget : public QWidget
+{
+    Q_OBJECT
+
+private:
+    // References to input fields where their contents can be
+    // grabbed at runtime.
+    QLineEdit* longitudeField = nullptr;
+    QLineEdit* latitudeField = nullptr;
+    QLineEdit* zoomField = nullptr;
+
+    // Setup functionality for map coordinate controls.
+    void setupInputFields(QBoxLayout* outerLayout);
+    void setupButtons(QBoxLayout* outerLayout, MapWidget* mapWidget);
+    void goButtonPressed();
+
+signals:
     /*!
-     * \brief The MapCoordControlWidget class is a collection
-     * of controls meant to modify the location and zoom of the
-     * MapWidgets viewport.
+     * \brief submitNewCoords
+     * This signal is emitted whenever the user presses the "Go" button.
+     *
+     * The function updates map coordinates to the new zoom, x, y position.
+     *
+     * \param x The x coordinate.
+     * \param y The y coordinate.
+     * \param zoom The zoom level.
      */
-    class MapCoordControlWidget : public QWidget {
-        Q_OBJECT
+    void submitNewCoords(double x, double y, double zoom);
 
-    public:
-        MapCoordControlWidget(MapWidget* parent);
-
-    private:
-        // References to our input fields so
-        // we can grab their contents at runtime.
-        QLineEdit* longitudeField = nullptr;
-        QLineEdit* latitudeField = nullptr;
-        QLineEdit* zoomField = nullptr;
-
-        void setupInputFields(QBoxLayout* outerLayout);
-        void setupButtons(QBoxLayout* outerLayout, MapWidget* mapWidget);
-
-    private:
-        void goButtonPressed();
-
-    signals:
-        /*!
-         * This signal is emitted whenever the user presses the "Go" button
-         */
-        void submitNewCoords(double x, double y, double zoom);
-    };
+public:
+    MapCoordControlWidget(MapWidget* parent);
+};
 }
 
 #endif // MAPCOORDCONTROLWIDGET_H
