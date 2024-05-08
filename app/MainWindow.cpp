388cb5ee9b7c833cb42746daa80bcddf2bc3def5
Cecilia Norevik Bratlie <cecilia_nor@hotmail.com>
2024-05-08 17:22:00 +0200

    docs: update documentation of MainWindow.cpp
    Functions where documentation was changed:
    * MainWindow::MainWindow(...)
    * MainWindow::resizeEvent(...)
    * MainWindow::showEvent(...)
    * MainWindow::updateControlsPositions()
    
    Updates: #121

--- app/MainWindow.cpp
+++ app/MainWindow.cpp
@@ -1,7 +1,7 @@
-// Qt header files
+// Qt header files.
 #include <QShowEvent>
 
-// Other header files
+// Other header files.
 #include "MainWindow.h"
 #include "MapCoordControlWidget.h"
 #include "MapPanControlWidget.h"
@@ -10,6 +10,16 @@
 
 using Bach::MainWindow;
 
+/*!
+ * \brief MainWindow::MainWindow
+ * Sets up the main window of the map application.
+ *
+ * The window is set to be 800x800 px at the start. The function sets up
+ * window controls, and allows users to input coordinates to move
+ * around the window.
+ *
+ * \param mapWidgetIn The input map widget.
+ */
 MainWindow::MainWindow(MapWidget* mapWidgetIn) : mapWidget{ mapWidgetIn }
 {
     resize(800, 800);
@@ -20,8 +30,8 @@ MainWindow::MainWindow(MapWidget* mapWidgetIn) : mapWidget{ mapWidgetIn }
     panControls = new MapPanControlWidget(mapWidget);
     renderControls = new MapRenderSettingsWidget(mapWidget);
 
-    // Setup the menu that lets us enter manual coordinates and hook it up to the
-    // map-widget.
+    // Set up the menu that lets the user enter manual coordinates and
+    // connect them to the map widget.
     MapCoordControlWidget *coordControls = new MapCoordControlWidget(mapWidget);
     QObject::connect(
         coordControls,
@@ -34,33 +44,55 @@ MainWindow::MainWindow(MapWidget* mapWidgetIn) : mapWidget{ mapWidgetIn }
     // Positioning of the hovering controls happens in the showEvent method.
 }
 
-void MainWindow::resizeEvent(QResizeEvent *event) {
+/*!
+ * \brief MainWindow::resizeEvent
+ * Handles resizing of the application window.
+ *
+ * \param event The event that triggers window resizing.
+ */
+void MainWindow::resizeEvent(QResizeEvent *event)
+{
     QMainWindow::resizeEvent(event);
     updateControlsPositions();
 }
 
-void MainWindow::showEvent(QShowEvent *event) {
+/*!
+ * \brief MainWindow::showEvent
+ * Shows/displays window controls at the correct position in a window.
+ *
+ * \param event The event that triggers controls to be shown.
+ */
+void MainWindow::showEvent(QShowEvent *event)
+{
     QMainWindow::showEvent(event);
     if (event->type() == QEvent::Show) {
         updateControlsPositions();
     }
 }
 
-void MainWindow::updateControlsPositions() {
-    // Position the panning controls
+/*!
+ * \brief MainWindow::updateControlsPositions
+ * Updates the positions of the floating control widgets (zooming and panning).
+ *
+ * updateControlsPositions is called every time the widget changes size.
+ * This updates the position of the control widgets
+ * so that they're placed correctly relative to this window.
+ */
+void MainWindow::updateControlsPositions()
+{
+    // Position the panning controls.
     if (panControls != nullptr) {
         panControls->move(
             0,
             height() - panControls->height());
     }
-
-    // Position the zoom controls
+    // Position the zooming controls.
     if (zoomControls != nullptr) {
         zoomControls->move(
             width() - zoomControls->width(),
             height() - zoomControls->height());
     }
-
+    // Position the rendering controls.
     if (renderControls != nullptr) {
         renderControls->move(
             width() - renderControls->width(),
