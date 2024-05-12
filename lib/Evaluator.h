6176b2cb9b21db6cfdfa6f10aee5865a517ad444
Cecilia Norevik Bratlie <cecilia_nor@hotmail.com>
2024-05-12 12:39:56 +0200

    refactor(evaluator): remove un-implemented func declarations
    The functions `equal` and `notEqual` are covered by the `compare` function and don't need their own implementation.
    
    Updates: #121

--- lib/Evaluator.h
+++ lib/Evaluator.h
@@ -20,14 +20,12 @@ private:
     static QVariant case_(const QJsonArray& array, const AbstractLayerFeature* feature, int mapZoomLevel, float vpZoomeLevel);
     static QVariant coalesce(const QJsonArray& array, const AbstractLayerFeature* feature, int mapZoomLevel, float vpZoomeLevel);
     static QVariant compare(const QJsonArray& array, const AbstractLayerFeature* feature, int mapZoomLevel, float vpZoomeLevel);
-    static QVariant equal(const QJsonArray& array, const AbstractLayerFeature* feature, int mapZoomLevel, float vpZoomeLevel);
     static QVariant get(const QJsonArray& array, const AbstractLayerFeature* feature, int mapZoomLevel, float vpZoomeLevel);
     static QVariant greater(const QJsonArray& array, const AbstractLayerFeature* feature, int mapZoomLevel, float vpZoomeLevel);
     static QVariant has(const QJsonArray& array, const AbstractLayerFeature* feature, int mapZoomLevel, float vpZoomeLevel);
     static QVariant in(const QJsonArray& array, const AbstractLayerFeature* feature, int mapZoomLevel, float vpZoomeLevel);
     static QVariant interpolate(const QJsonArray& array, const AbstractLayerFeature* feature, int mapZoomLevel, float vpZoomeLevel);
     static QVariant match(const QJsonArray& array, const AbstractLayerFeature* feature, int mapZoomLevel, float vpZoomeLevel);
-    static QVariant notEqual(const QJsonArray& array, const AbstractLayerFeature* feature, int mapZoomLevel, float vpZoomeLevel);
 
 public:
     Evaluator(){};
